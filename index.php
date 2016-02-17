44b6f1f0ca66796206a0813f293c80056bcf687c
Dale Mooney <dale.mooney@asset.tv>
2016-02-17 06:24:09 -0500

    Applied fixes from StyleCI

--- index.php
+++ index.php
@@ -4,16 +4,14 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 
 //AUTO LOAD
-include("MediaManager/autoload.php");
+include 'MediaManager/autoload.php';
 
 //CREATE MEDIAMANAGER INSTANCE
-$MediaManager = new \MediaManager\MediaManager("demo", "{apikey}");
-
+$MediaManager = new \MediaManager\MediaManager('demo', '{apikey}');
 
 //ADD A PLAYLIST FILTER
-$MediaManager->API->addPlaylistFilter("{playlist}");
-$MediaManager->API->addTemplateFilter("{template}");
+$MediaManager->API->addPlaylistFilter('{playlist}');
+$MediaManager->API->addTemplateFilter('{template}');
 
 //GET CLIENT
 $videos = $MediaManager->API->getVideos();
-
