3c64b39897e32b6776f287c98657f6773e1f6933
Dale Mooney <dale.mooney@asset.tv>
2016-02-23 14:43:45 +0000

    From and To are now QueryDate objects and can be set via methods. It will also default.

--- src/MediaManager/Analytics/Query.php
+++ src/MediaManager/Analytics/Query.php
@@ -3,12 +3,17 @@
 namespace MediaManager\Analytics;
 
 /**
- * Description of Query.
- *
- * @author Dale
+ * A Query object which can be used to generate MMQL.
  */
 class Query
 {
+
+    /**
+     * The to and from dates.
+     * @var \MediaManager\Analytics\QueryDate 
+     */
+    private $to, $from;
+
     /**
      * The Show object.
      *
@@ -21,8 +26,14 @@ class Query
      */
     public function __construct()
     {
-        //SET THE DEFAULT SHOW
+        //Set the default show query
         $this->Show = new Show('Video');
+
+        //Set from date to now.
+        $this->from = new \MediaManager\Analytics\QueryDate("now");
+
+        //Set to date to now.
+        $this->to = new \MediaManager\Analytics\QueryDate("now");
     }
 
     /**
@@ -37,6 +48,60 @@ class Query
         $this->Show = new Show($show);
 
         return $this->Show;
+    }
+
+    /**
+     * Get the Show Query.
+     * @return Show
+     */
+    public function get()
+    {
+        return $this->Show;
+    }
+
+    /**
+     * Get the Show Query.
+     * @return Show
+     */
+    public function getSow()
+    {
+        return $this->get();
+    }
+
+    /**
+     * Set the from date
+     * @param \MediaManager\Analytics\QueryDate $from
+     */
+    public function setFrom(\MediaManager\Analytics\QueryDate $from)
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * Set the to date.
+     * @param \MediaManager\Analytics\QueryDate $to
+     */
+    public function setTo(\MediaManager\Analytics\QueryDate $to)
+    {
+        $this->to = $to;
+    }
+
+    /**
+     * Get the from QueryDate
+     * @return \MediaManager\Analytics\QueryDate
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    /**
+     * Get the to QueryDate
+     * @return \MediaManager\Analytics\QueryDate
+     */
+    public function getTo()
+    {
+        return $this->to;
     }
 
     /**
@@ -60,6 +125,6 @@ class Query
      */
     public function __toString()
     {
-        return ''.$this->Show;
+        return '' . $this->Show;
     }
 }
